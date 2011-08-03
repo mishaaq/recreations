8d11b3fbcae597aab8a52af0cf46659c31d26982
Michał Żarłok <mishaaq@gmail.com>
2011-08-03 21:43:54 +0200

    refactoring, billard :)

--- foosball.rb
+++ foosball.rb
@@ -13,35 +13,104 @@ use Rack::Flash
 
 resolv = Resolv.new
 
-today = nil
-times = %w[8:00 8:30 9:00 9:30 10:00 10:30 11:00 11:30 12:00 12:30 13:00 13:30 14:00 14:30 15:00 15:30 16:00 16:30 17:00]
+class Recreation
+  def initialize(label, opening_hours, reservations_per_user=1)
+    @label = label
+    @opening_hours = opening_hours.clone.freeze
+    @reservations_per_user = reservations_per_user || 1
+    @reservations = {}
+  end
 
-reservations = nil
-
-before do
-  if today != Date.today
-    reservations = Hash[times.zip([nil])]
-    today = Date.today
+  def reserve(name, hour)
+    reservations_made = @reservations.values.select {|reservation| reservation == name}
+    if reservations_made.length < @reservations_per_user
+      if @reservations[hour].nil?
+        @reservations[hour] = name
+      else
+        raise ReservationException.new("Ouch! Someone has made a reservation on this time in this moment.")
+      end
+    else
+      raise ReservationException.new("You have already made a maximum number of reservation today.")
+    end
+  end
+  
+  def cancel(name, hour)
+    if name == @reservations[hour]
+      @reservations[hour] = nil
+    else
+      raise ReservationException.new("You've tried to cancel someone's else reservation!")
+    end
+  end
+  
+  def label
+    @label
+  end
+  
+  def reservations    
+    Marshal::load(Marshal.dump(@reservations)).freeze
+  end
+  
+  def opening_hours
+    @opening_hours
+  end
+  
+  class ReservationException < Exception
   end
 end
 
+class RecreationFactory
+  def self.create(label, hours, minutes, max_per_user=nil)
+    opening_hours = (hours.map {|h| minutes.map {|m| h.to_s+":"+m.to_s}}).flatten
+    Recreation.new(label, opening_hours, max_per_user)
+  end
+end
+
+today = nil
+reservations = []
+name = ""
+
+before do
+  if today != Date.today
+    foosball = RecreationFactory.create("Foosball", (8..16), ["00", "30"])
+    billard = RecreationFactory.create("Billard", (8..16), %w[00 15 30 45], 2)
+    reservations = [foosball, billard]
+    today = Date.today
+  end
+  name = resolv.getname(request.ip).split('.').first
+  name = request.host if name == 'localhost'
+end
+
 get '/' do
-  available = times.select { |time| reservations[time].nil? }
-  haml :index, :locals => { :times => times, :available => available, :reservations => reservations,
+  haml :index, :locals => { :recreations => reservations, :name => name,
                             :notice => flash[:notice], :error => flash[:error] }
 end
 
-post '/' do
-  name = resolv.getname(request.ip)
-  unless reservations.has_value?(name)
-    if reservations[params[:time]].nil?
-      reservations[params[:time]] = name
-      flash[:notice] = "Reservation made."
-    else
-      flash[:error] = "Ouch! Someone has made a reservation on this time in this moment."
-    end
-  else
-    flash[:error] = "You have already made a reservation today."
+post '/reserve' do
+  begin
+    reservation_id = params[:id] or raise "No reservation ID!"
+    reservations[reservation_id.to_i].reserve(name, params[:time])
+    flash[:notice] = "Reservation made."
+  rescue Recreation::ReservationException => e
+    flash[:error] = e.message
+  rescue Exception => e
+    puts e.message
+    puts e.backtrace.join("\n")
+    flash[:error] = "The application badly behaved :(."
+  end
+  redirect to('/')
+end
+
+post '/cancel' do
+  begin
+    reservation_id = params[:id] or raise "No reservation ID!"
+    reservations[reservation_id.to_i].cancel(name, params[:time])
+    flash[:notice] = "Reservation canceled."
+  rescue Recreation::ReservationException => e
+    flash[:error] = e.message
+  rescue Exception => e
+    puts e.message
+    puts e.backtrace.join("\n")
+    flash[:error] = "The application badly behaved :(."
   end
   redirect to('/')
 end
@@ -57,7 +126,7 @@ __END__
 !!!
 %html(xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en")
   %head
-    %title= "Foosball"
+    %title= "Recreations"
     %link{:rel => "stylesheet", :href => "/stylesheet.css", :type => "text/css"}
   %body
     = yield
@@ -70,23 +139,41 @@ __END__
   - if error
     %div.error
       %p= error
-%div.table
+%div.tables
   %table
     %thead
       %tr
-        %th Hour
-        %th Occupied
+        - recreations.each do |recreation|
+          %td.label= recreation.label.capitalize
     %tbody
-      - times.each do |time|
-        %tr
-          %td= time
-          %td
-            - if reservations[time].nil?
-              %form{:action => '/', :method => :post}
-                %input{:type => :hidden, :name => 'time', :value => time}
-                %input{:type => :submit, :value => "Reserve"}
-            - else
-              = reservations[time]
+      %tr
+        - recreations.each_with_index do |recreation, index|
+          - reservations = recreation.reservations
+          %td.recreation
+            - recreation.opening_hours.each_slice(18) do |time_slice|
+              %table.recreation
+                %thead
+                  %tr
+                    %th Hour
+                    %th Occupied
+                %tbody
+                  - time_slice.each do |time|
+                    %tr
+                      %td= time
+                      %td
+                        - if reservations[time].nil?
+                          %form{:action => '/reserve', :method => :post}
+                            %input{:type => :hidden, :name => 'id', :value => index}
+                            %input{:type => :hidden, :name => 'time', :value => time}
+                            %input{:type => :submit, :value => "Reserve"}
+                        - else
+                          = reservations[time]
+                          - if reservations[time] == name
+                            %form.cancel{:action => '/cancel', :method => :post}
+                              %input{:type => :hidden, :name => 'id', :value => index}
+                              %input{:type => :hidden, :name => 'time', :value => time}
+                              %button{:type => :submit}
+                                %img{:src => "/cross.png"}
 
 %div.graphic
   %img{:src => "/table.jpg"}
@@ -104,11 +191,20 @@ __END__
     &.error
       background-color: red
   
-.table
+.tables
   margin: 20px
   float: left
-
+  
 table
+  td.recreation
+    vertical-align: top
+    padding: 5px
+  td.label
+    text-align: center
+    font-weight: bold
+
+table.recreation
+  display: inline-table
   border-color: gray
   th
     vertical-align: middle
@@ -137,3 +233,9 @@ table
 .graphic
   float: right
   margin: 20px
+
+form.cancel
+  display: inline
+
+button
+  width: auto
