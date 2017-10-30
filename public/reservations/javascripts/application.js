fb1d280cdbff7103f601c33aaf8dbe09fd93ccb9
Michał Żarłok <mishaaq@gmail.com>
2017-10-30 13:34:37 +0100

    fix for xl, anchor scrolling, fix nav links in one line

--- public/reservations/javascripts/application.js
+++ public/reservations/javascripts/application.js
@@ -1,1 +1,18 @@
-// Put your application scripts here
+$(document).ready(function () {
+
+  var $root = $('html, body');
+
+  $(document).on('click', '.navbar-nav a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, function () {
+      window.location.hash = href;
+    });
+
+    return false;
+  });
+});
