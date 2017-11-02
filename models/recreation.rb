73f93a61d339a14116bf9142d3827b8abb565762
Michał Żarłok <mishaaq@gmail.com>
2017-11-02 11:55:49 +0100

    add validations

--- models/recreation.rb
+++ models/recreation.rb
@@ -8,5 +8,8 @@ class Recreation
   has n, :reservations, :constraint => :destroy
   has 1, :reservation_settings, :constraint => :destroy
 
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
   accepts_nested_attributes_for :reservation_settings, :allow_destroy => true
 end
