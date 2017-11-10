23cab73e64bf15891617b03f1a9c9c62ffd6fdf5
Michał Żarłok <mishaaq@gmail.com>
2017-11-10 10:33:49 +0100

    try to fix outlook subscribers

--- reservations/controllers/calendars.rb
+++ reservations/controllers/calendars.rb
@@ -5,27 +5,24 @@ Recreations::Reservations.controllers :calendars do
     if user.nil?
       halt 404
     end
-    reservations = user.reservations.today
-    calendar = Icalendar::Calendar.new
-    calendar.x_wr_calname = 'Reservations'
-    reservations.each do |r|
-      time_interval = r.recreation.reservation_settings.for_time
-      calendar.event do |e|
-        e.dtstart  = r.time
-        e.dtend    = r.time.advance({:hours => time_interval.hour, :minutes => time_interval.minute})
-        e.summary  = "Reservation for #{r.recreation.name.downcase}."
-        e.url      = absolute_url(:reservations, :index) + "##{reservation_anchor(r)}"
-        e.alarm do |a|
-          a.action  = 'DISPLAY'
-          a.summary = "You have a reservation for #{r.recreation.name.downcase}."
-          a.trigger = '-PT5M'
-        end
-      end
-    end
-    calendar.publish
+
+    calendar = Recreations::ReservationsCalendar.new
+    user.reservations.each {|r| calendar << r }
 
     content_type 'text/calendar'
     calendar.to_ical
   end
 
+  get :ical, :with => :id do
+    reservation = Reservation.get(params[:id])
+    if reservation.nil?
+      halt 404
+    end
+
+    calendar = Recreations::ReservationsCalendar.new
+    calendar << reservation
+
+    content_type 'text/calendar'
+    calendar.to_ical
+  end
 end
--- reservations/helpers/calendars_helper.rb
+++ reservations/helpers/calendars_helper.rb
@@ -1,6 +1,7 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 require 'uri'
+require 'icalendar/tzinfo'
 
 module Recreations
   class Reservations
@@ -16,5 +17,39 @@ module Recreations
     end
 
     helpers CalendarsHelper
+  end
+
+  class ReservationsCalendar
+
+    def initialize
+      @calendar = Icalendar::Calendar.new
+      @calendar.x_wr_calname = 'Reservations'
+      tzid = Time.zone.name
+      timezone = TZInfo::Timezone.get(tzid)
+      @calendar.add_timezone(timezone.ical_timezone(Time.now))
+    end
+
+    def <<(r)
+      time_interval = r.recreation.reservation_settings.for_time
+      @calendar.event do |e|
+        e.dtstart     = Icalendar::Values::DateTime.new(r.time, :tzid => Time.zone.name)
+        e.dtend       = Icalendar::Values::DateTime.new(r.time.advance({:hours => time_interval.hour,
+                                                                              :minutes => time_interval.minute}), :tzid => Time.zone.name)
+        e.summary     = "Reservation for #{r.recreation.name.downcase}."
+        #e.url         = absolute_url(:reservations, :index) + "##{reservation_anchor(r)}"
+        e.organizer   = "Reservation System"
+        e.alarm do |a|
+          a.action      = 'DISPLAY'
+          a.summary     = "Reservation for #{r.recreation.name.downcase}."
+          a.description = "You have reserved #{r.recreation.name.downcase}."
+          a.trigger     = '-PT5M'
+        end
+      end
+    end
+
+    def to_ical
+      @calendar.publish
+      @calendar.to_ical
+    end
   end
 end
